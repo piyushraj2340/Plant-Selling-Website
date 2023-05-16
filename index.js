2f93a3e81ce4c704095708c9b4c3c1b44ac828e4
piyushraj2340 <piyushraj2340@gmail.com>
2023-05-16 19:20:08 +0530

    add domain of the deployent

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser');
 const port = process.env.port || 8000;
 const app = express();
 
-const YOUR_DOMAIN = 'http://localhost:8000';
+const YOUR_DOMAIN = 'https://plant-selling-website-backend.vercel.app/';
 
 
 app.use(express.json());
