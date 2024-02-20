5d704e405c2bd1b102d933fd82da03a636928d2c
piyushraj2340 <piyushraj2340@gmail.com>
2024-02-21 03:48:15 +0530

    created the react app for plantselling website

---
+++ client/frontend/src/Controller/template.js
@@ -0,0 +1,286 @@
+const completeSection = {
+    templateId: "completeSection",
+    href: "",
+    images: [{
+        url: '',
+        public_id: "",
+    }]
+}
+
+const twoSection = {
+    templateId: "twoSection",
+    href: "",
+    images: [
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        }
+    ]
+}
+
+const fourSection = {
+    templateId: "fourSection",
+    href: "",
+    images: [
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        }
+    ]
+}
+
+const twoSectionAndRightVertical = {
+    templateId: "twoSectionAndRightVertical",
+    href: "",
+    images: [
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        }
+    ]
+}
+const twoSectionAndRightFour = {
+    templateId: "twoSectionAndRightFour",
+    href: "",
+    images: [
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        }
+    ]
+}
+const twoSectionAndLeftVertical = {
+    templateId: "twoSectionAndLeftVertical",
+    href: "",
+    images: [
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        }
+    ]
+}
+const twoSectionAndLeftFour = {
+    templateId: "twoSectionAndLeftFour",
+    href: "",
+    images: [
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        },
+        {
+            url: '',
+            public_id: "",
+        }
+    ]
+}
+
+
+export {completeSection, twoSection, fourSection, twoSectionAndRightVertical, twoSectionAndRightFour, twoSectionAndLeftVertical, twoSectionAndLeftFour};
+
+
+
+// {
+//     _id: "1234569",
+//     tabName: "Home",
+//     renders: [
+//         {
+//             _id: "completeSection",
+//             templateId: "completeSection",
+//             href: "/completeSection",
+//             images: [{
+//                 url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707424265/Test%20Image%20Upload/yl4o1n1cidp71fpbd3lp.png',
+//                 public_id: "c7f6f0366792a6b3984af1e8fe7a6d18",
+//             }]
+//         },
+//         {
+//             _id: "twoSection",
+//             templateId: "twoSection",
+//             href: "/twoSection",
+//             images: [
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707427739/Test%20Image%20Upload/c1ewnsiz8yq400uba2mr.jpg',
+//                     public_id: "c7f6f0366792a6b3984af1e8fe7a6d18",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707427740/Test%20Image%20Upload/dmwceoxfw5hxonxxhnsd.jpg',
+//                     public_id: "",
+//                 }
+//             ]
+//         },
+//         {
+//             _id: "fourSection",
+//             templateId: "fourSection",
+//             href: "/fourSection",
+//             images: [
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/xlbproqv0nbxncilhwgh.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/xkps0jatmv5voc6bansg.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/ewfwj7gzpwgyeclldoex.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/rerpjg6yg2rhmzvouexz.jpg',
+//                     public_id: "",
+//                 }
+//             ]
+//         },
+//         {
+//             _id: "twoSectionAndRightVertical",
+//             templateId: "twoSectionAndRightVertical",
+//             href: "/twoSectionAndRightVertical",
+//             images: [
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707427739/Test%20Image%20Upload/c1ewnsiz8yq400uba2mr.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707424265/Test%20Image%20Upload/z4fsb9afjhhcltnphyxp.png',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707424264/Test%20Image%20Upload/whyho2bn3tnildjwn4f2.png',
+//                     public_id: "",
+//                 }
+//             ]
+//         },
+//         {
+//             _id: "twoSectionAndRightFour",
+//             templateId: "twoSectionAndRightFour",
+//             href: "/twoSectionAndRightFour",
+//             images: [
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707427739/Test%20Image%20Upload/c1ewnsiz8yq400uba2mr.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/ewfwj7gzpwgyeclldoex.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/rerpjg6yg2rhmzvouexz.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/rerpjg6yg2rhmzvouexz.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/ewfwj7gzpwgyeclldoex.jpg',
+//                     public_id: "",
+//                 }
+//             ]
+//         },
+//         {
+//             _id: "twoSectionAndLeftVertical",
+//             templateId: "twoSectionAndLeftVertical",
+//             href: "/twoSectionAndLeftVertical",
+//             images: [
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707424265/Test%20Image%20Upload/z4fsb9afjhhcltnphyxp.png',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707424264/Test%20Image%20Upload/whyho2bn3tnildjwn4f2.png',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707427739/Test%20Image%20Upload/c1ewnsiz8yq400uba2mr.jpg',
+//                     public_id: "",
+//                 }
+//             ]
+//         },
+//         {
+//             _id: "twoSectionAndLeftFour",
+//             templateId: "twoSectionAndLeftFour",
+//             href: "/twoSectionAndLeftFour",
+//             images: [
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/ewfwj7gzpwgyeclldoex.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/rerpjg6yg2rhmzvouexz.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/rerpjg6yg2rhmzvouexz.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707428298/Test%20Image%20Upload/ewfwj7gzpwgyeclldoex.jpg',
+//                     public_id: "",
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/dcd6y2awx/image/upload/v1707427739/Test%20Image%20Upload/c1ewnsiz8yq400uba2mr.jpg',
+//                     public_id: "",
+//                 }
+//             ]
+//         }
+//     ]
+// }
