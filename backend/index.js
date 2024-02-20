a3bdc1becbb004f3910d35304fe77e2e5f301df9
piyushraj2340 <piyushraj2340@gmail.com>
2024-02-21 04:28:49 +0530

    changed the folder structure and now testing the department

--- backend/index.js
+++ backend/index.js
@@ -92,16 +92,16 @@ app.use("/api/v2", payment);
 
 
 if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.resolve(__dirname, 'client', 'frontend' , 'build')));
+    app.use(express.static(path.resolve(__dirname, 'client' , 'build')));
     app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'frontend' , 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client' , 'build', 'index.html'));
     })
 } else {
-    app.use(express.static(path.resolve(__dirname, 'client', 'frontend' , 'build')));
+    app.use(express.static(path.resolve(__dirname, 'client' , 'build')));
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 })
 
 
