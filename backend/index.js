fccbb4bdf12f864ba1b938d7e96624d239eda0e0
piyushraj2340 <piyushraj2340@gmail.com>
2024-02-25 05:01:25 +0530

    removed the unused commented code

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-// const stripe = require('stripe')('sk_test_51Mh3nrSG2OBM7gGqYliQervsXmac5hUFnGL7QTvcMDGR660smHtvnX7ashBsvy21gC4Aq6hX4bPPwJ8fjwnj7SMJ00ksDhcser');
 require('dotenv').config();
 require('./src/db/db');
 
@@ -55,56 +54,6 @@ app.use("/api/v2/products", products);
 
 
 
-
-// after testing delete this route
-// app.use("/api/v2", nurseryStoreRoute);
-// app.use("/api/v2", plantsRoute);
-// app.use("/api/v2", cart);
-// app.use("/api/v2", address);
-// app.use("/api/v2", payment);
-
-// app.post("/payments", async (req, res) => {
-//     try {
-//         const data = req.body;
-//         // console.log(data);
-
-//         const session = await stripe.checkout.sessions.create({
-//             line_items: [
-//                 {
-//                     price_data: {
-//                         currency: "INR",
-//                         product_data: {
-//                             name: data.plantsName,
-//                         },
-//                         unit_amount: (Math.round(data.price - data.discount / 100 * data.price)) * 100
-//                     },
-//                     quantity: data.quantity,
-//                 }
-//             ],
-//             mode: 'payment',
-//             success_url: `${YOUR_DOMAIN}/success`,
-//             cancel_url: `${YOUR_DOMAIN}/cancel`,
-//         });
-
-//         const result = {
-//             status: true,
-//             message: "Transition in progress!...",
-//             link: session.url
-//         }
-
-//         res.send(result);
-//     } catch (error) {
-//         const result = {
-//             status: false,
-//             message: "Something Went Wrong!...",
-//         }
-
-//         console.log(error);
-//         res.send(result);
-//     }
-// })
-
-
 // if (process.env.NODE_ENV == 'production') {
 //     app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 //     app.get('/', (req, res) => {
@@ -119,9 +68,9 @@ app.use("/api/v2/products", products);
 // })
 
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.status(200).send("Welcome to Plant Selling Website." + "<br />" + "Frontend App: " + `<a href="${process.env.FRONTEND_URL}" target="_blank">${process.env.FRONTEND_URL}</a>`);
-})
+});
 
 app.listen(port, () => {
     console.log("listening to port 8000");
