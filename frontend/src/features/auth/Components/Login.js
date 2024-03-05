c7e8f6dcc2c996acbef38e3854f8302deb9718e6
piyushraj2340 <piyushraj2340@gmail.com>
2024-03-06 00:37:20 +0530

    refactor: Restructure the Login page
    - Reorganized folder structure to /src/pages for better organization
    - Moved the Login Components to the appropriate folders under the /src/pages based on web pages
    - Introduced reusable components for Login Pages in /src/features/auth

---
+++ frontend/src/features/auth/Components/Login.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { message } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { userLoginAsync } from '../authSlice';
+import { setCart } from '../../cart/cartSlice'; // TODO: IMPLEMENTATION: CART FUNCTIONALITY
+
+
+function Login() {
+    const userLoggedInStatus = useSelector(state => state.auth.userLoggedInStatus);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handelUserLoginStatus = () => {
+        if (userLoggedInStatus && userLoggedInStatus.status) {
+            message.success(userLoggedInStatus.message);
+
+            const [redirect, to] = window.location.search && window.location.search.split("=");
+            navigate(redirect === "?redirect" ? to : "/profile");
+
+            return;
+        } else if (userLoggedInStatus && !userLoggedInStatus.status) {
+            message.error(userLoggedInStatus.message);
+
+            return;
+        }
+    }
+
+    useEffect(() => {
+        handelUserLoginStatus();
+    }, [dispatch, userLoggedInStatus]);
+
+    const [userFormData, setUserFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    let name, value;
+    const handleInputs = (e) => {
+        name = e.target.name;
+        value = e.target.value;
+
+        setUserFormData({ ...userFormData, [name]: value });
+    }
+
+    const handleUserLogin = async (e) => {
+        e.preventDefault();
+        if (userFormData.email === "" || userFormData.password === "") { // if one of the empty fields.
+            message.error("Please enter your credentials.")
+            return;
+        }
+        dispatch(userLoginAsync(userFormData));
+    }
+
+    return (
+        <div className='d-flex justify-content-center py-2 px-2 mb-4 mb-md-5'>
+            <div className='col-12 col-md-8 col-lg-6 col-xl-4 shadow border rounded px-2 py-2 p-md-5'>
+                <div className="d-flex flex-column flex-md-row justify-content-center">
+                    <div className='col-12 col-md-6 text-center p-0 mb-2 mb-md-0 me-md-2 bg-primary rounded'>
+                        <Link to={"/login"} className='btn text-light w-100'>Login</Link>
+                    </div>
+                    <div className='col-12 col-md-6 text-center p-0 ms-md-2 bg-secondary rounded'>
+                        <Link to={"/signup"} className='btn text-light w-100'>Signup</Link>
+                    </div>
+                </div>
+                <div className="row p-3">
+                    <p className="text-center m-0 ">Connect With Social Account: </p>
+                </div>
+                <div className="row p-3">
+                    <p className="text-center login-social-link m-0">
+                        <i className="fab fa-facebook-f ms-4 cursor-pointer"></i>
+                        <i className="fab fa-google ms-4"></i>
+                        <i className="fab fa-twitter ms-4"></i>
+                        <i className="fab fa-github ms-4"></i>
+                    </p>
+                </div>
+                <div className="row">
+                    <p className="text-center">Or:</p>
+                </div>
+
+                <form onSubmit={handleUserLogin}>
+                    <div className="d-flex justify-content-center">
+                        <div className="col-12">
+                            <input type="email" onChange={handleInputs} className='form-control mb-3' name="email" id="email" placeholder='Enter Email' />
+                            <input type="password" onChange={handleInputs} className='form-control mb-2' name="password" id="password" placeholder='Enter Password' value={userFormData.password} />
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-end p-2">
+                        <p className="m-0">
+                            <Link to={"/user/forgot-password"}>Forgot Password?</Link>
+                        </p>
+                    </div>
+                    <div className="justify-content-center mt-2">
+                        <div className="col-12">
+                            <button className='btn btn-primary w-100' type="submit">Login</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import Login from '../features/auth/Components/Login';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { userProfileAsync } from '../features/user/userSlice';
+
+
+const LoginPage = () => {
+    document.title = "Login";
+
+    const user = useSelector(state => state.user.user);
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const handleGetUserData = async () => {
+        dispatch(userProfileAsync());
+        if (user) {
+            const [redirect, to] = window.location.search && window.location.search.split("=");
+            navigate(redirect === "?redirect" ? to : "/profile");
+            return;
+        }
+    }
+
+    useEffect(() => {
+        handleGetUserData();
+    }, [dispatch, user]);
+
+    return (
+        <Login />
+    )
+}
+
+export default LoginPage
