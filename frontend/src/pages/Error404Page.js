4c161762744f4f7f7cbdad7359c1d46ee5d3a1a0
piyushraj2340 <piyushraj2340@gmail.com>
2024-03-06 21:02:53 +0530

    refactor: move ContactUs, Error404 Components to /src/pages based on web pages

---
+++ frontend/src/pages/Error404Page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Error404 = () => {
+  document.title = "Page Not Found"
+  const error404 = "https://res.cloudinary.com/dcd6y2awx/image/upload/f_auto,q_auto/v1/Static%20PlantSeller%20Project%20Images/404";
+  return (
+    <section className='bg-section p-3 p-md-5'>
+      <div className='d-flex justify-content-center align-item-center' style={{ height: "50vh" }}>
+        <img src={error404} alt="Page not found" className='img-fluid' />
+      </div>
+      <h1 className='text-center py-1 py-md-2' style={{fontFamily: "cursive"}}>Page Not Found!</h1>
+    </section>
+
+  )
+}
+
+export default Error404
