ab0105fe52eab2d32373786e662c72d516b8df2d
piyushraj2340 <piyushraj2340@gmail.com>
2024-03-06 20:38:08 +0530

    refactor: Restructure the Logout page
    - Reorganized folder structure to /src/pages for better organization
    - Moved the Profile Components to the appropriate folders under the /src/pages based on web pages
    - Introduce reusable components for Profile Pages in /src/features/auth/Components and
    - Remove helper function in the src/features/auth/components/utils

---
+++ frontend/src/pages/LogoutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logout from '../features/auth/Components/Logout';
+
+function LogoutPage() {
+    document.title = "Logout";
+    return (
+        <Logout />
+    )
+}
+
+export default LogoutPage
