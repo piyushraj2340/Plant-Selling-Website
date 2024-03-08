614b961ec049caf5ee21617b792c5a89d33d135d
piyushraj2340 <piyushraj2340@gmail.com>
2024-03-08 21:03:17 +0530

    refactor: Restructure the Cart Page
    - Reorganized folder structure to /src/pages for better organization
    - Moved the Cart Components to the appropriate folders under the /src/pages based on web page
    - Rename the Cart Components to CartPage
    - Introduce reusable components for CartPage in /src/features/cart/Components

--- frontend/src/features/cart/Components/Cart.js
+++ frontend/src/features/cart/Components/Cart.js
@@ -1,169 +1,83 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { UserContext } from '../App';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import AddressList from './Shared/AddressList';
-import noDataFound from '../Asset/img/noDataFound.jpg';
-import handelDataFetch from '../utils/handelDataFetch';
+import AddressList from '../../../Components/Shared/AddressList';
 import { message } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { cartDataDeleteAsync, cartDataFetchAsync, cartDataUpdateQuantityAsync, setCartPricing } from '../cartSlice';
+import { addressListDataFetchAsync, setSelectedAddress } from '../../address/addressSlice';
 
 function Cart() {
-  document.title = "Cart Information";
 
-  const { setCartLength, setShowAnimation } = useContext(UserContext);
+  const cart = useSelector(state => state.cart.carts);
+  const addressList = useSelector(state => state.address.addressList);
+  const selectedAddress = useSelector(state => state.address.selectedAddress);
+  const cartPriceDetails = useSelector(state => state.cart.cartPriceDetails);
 
-  const [cart, setCart] = useState([]);
-  const [pricing, setPricing] = useState(null);
+  const dispatch = useDispatch();
 
-  const [address, setAddress] = useState(null);
-  const [addressList, setAddressList] = useState(null);
   const [viewAddressList, setViewAddressList] = useState(false);
 
+  const noDataFound = "https://res.cloudinary.com/dcd6y2awx/image/upload/f_auto,q_auto/v1/PlantSeller/UI%20Images/no-data-found";
 
   const navigate = useNavigate();
 
-  const handelGetDefaultAddress = async () => {
-    try {
-      const result = await handelDataFetch({ path: '/api/v2/user/default/address', method: "GET" }, setShowAnimation);
-
-      if (result.status) {
-        setAddress(result.result);
-      } else {
-        throw new Error(result.message);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  const handelCalculatePricing = (productArray) => {
-    const totalPriceWithoutDiscount = (productArray.reduce((total, curObj) => total + (curObj.pricing.priceWithoutDiscount * curObj.quantity), 0).toFixed(2));
-    const actualPriceAfterDiscount = (productArray.reduce((total, curObj) => total + (curObj.pricing.priceAfterDiscount * curObj.quantity), 0)).toFixed(2);
-    const discountPrice = (productArray.reduce((total, curObj) => total + (curObj.pricing.discountPrice * curObj.quantity), 0)).toFixed(2);
-    const deliveryPrice = (actualPriceAfterDiscount < 500 ? 90 : 0).toFixed(2)
-
-    const pricing = {
-      totalPriceWithoutDiscount,
-      actualPriceAfterDiscount,
-      discountPrice,
-      deliveryPrice, // calculate the delivery price dynamic
-      totalPrice: (Number(actualPriceAfterDiscount) + Number(deliveryPrice)).toFixed(2)
-    }
-    setPricing(pricing);
-  }
-
-  const handleGetAddedCart = async () => {
-    try {
-      const result = await handelDataFetch({ path: '/api/v2/checkout/carts', method: "GET" }, setShowAnimation);
-
-      if (result.status) {
-        // const cartValues = result.result.reduce((total, curObj) => total + ((curObj.plant.price - curObj.plant.discount / 100 * curObj.plant.price) * curObj.quantity), 0);
-        handelCalculatePricing(result.result);
-        setCart(result.result);
-        setCartLength({ type: "CART", length: result.result.length });
-      } else {
-        setCart([]);
-        setPricing(null);
-        throw new Error(result.message);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   useEffect(() => {
-    handleGetAddedCart();
-    handelGetDefaultAddress();
-  }, []);
+    !cart.length && dispatch(cartDataFetchAsync());
+    !addressList.length && dispatch(addressListDataFetchAsync());
+    dispatch(setCartPricing());
+  }, [dispatch, cart]);
 
-  const handelGetListOfAddress = async () => {
-    try {
-
-      const result = await handelDataFetch({ path: "/api/v2/user/address", method: "GET" }, setShowAnimation);
-
-      if (result) {
-        setAddressList(result.result);
-      } else {
-        throw new Error(result.message);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  const setSelectedAddress = (_id) => {
+  const handelSelectedAddress = (_id) => {
     let address = addressList.filter((elem) => {
       return elem._id === _id;
     });
-    setAddress(address[0]);
+    dispatch(setSelectedAddress(address[0]));
     setViewAddressList(!viewAddressList);
   }
 
   const handleDeleteFromCart = async (cartId) => {
-    try {
-      const result = await handelDataFetch({ path: `/api/v2/checkout/carts/${cartId}`, method: "DELETE" }, setShowAnimation);
-
-      if (result.status) {
-        setCartLength({ type: "CART", length: result.cartLength });
-        handleGetAddedCart();
-      } else {
-        handleGetAddedCart();
-        setCartLength({ type: "CART", length: null });
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    dispatch(cartDataDeleteAsync(cartId));
+    dispatch(setCartPricing());
   }
 
-  const handleUpdateCart = async (cartId, plant, quantity, price, discount) => {
-    try {
-      const result = await handelDataFetch({ path: `/api/v2/checkout/carts/${cartId}`, method: "PATCH", body: { plant, quantity, addedAtPrice: Math.round(price - discount / 100 * price) } }, setShowAnimation);
-
-      if (result.status) {
-        setCartLength({ type: "CART", length: result.result.length });
-        handleGetAddedCart();
-      } else {
-        setCartLength({ type: "CART", length: null });
-        handleGetAddedCart();
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  const handleUpdateCart = async (cartId, quantity) => {
+    dispatch(cartDataUpdateQuantityAsync({cartId, quantity}))
   }
 
   const handelBuyProduct = async () => {
-    try {
-      const data = {
-        cartOrProducts: cart,
-        pricing,
-        shippingInfo: address
-      }
+    // try {
+    //   const data = {
+    //     cartOrProducts: cart,
+    //     pricing,
+    //     shippingInfo: address
+    //   }
 
-      const result = await handelDataFetch({ path: "/api/v2/checkout", method: "POST", body: data }, setShowAnimation);
+    //   const result = await handelDataFetch({ path: "/api/v2/checkout", method: "POST", body: data }, setShowAnimation);
 
-      message.config({
-        top: 100,
-        maxCount: 3,
-        CSSProperties: {
-          backgroundColor: "#000",
-          color: "#fff"
-        }
-      })
+    //   message.config({
+    //     top: 100,
+    //     maxCount: 3,
+    //     CSSProperties: {
+    //       backgroundColor: "#000",
+    //       color: "#fff"
+    //     }
+    //   })
 
-      if (result.status) {
+    //   if (result.status) {
 
-        if (address) {
-          navigate("/checkout/confirm");
-        } else {
-          navigate("/checkout/shipping");
-        }
-      } else {
+    //     if (address) {
+    //       navigate("/checkout/confirm");
+    //     } else {
+    //       navigate("/checkout/shipping");
+    //     }
+    //   } else {
 
-        message.error("An error occurred during order processing");
-      }
+    //     message.error("An error occurred during order processing");
+    //   }
 
-    } catch (error) {
-      console.log(error);
-    }
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 
   return (
@@ -206,7 +120,7 @@ function Cart() {
                               </div>
                               <div className='m-0 d-flex small'>
                                 <p className="m-0 d-none-md">Quantity: </p>
-                                <select value={elem.quantity} onChange={(e) => { handleUpdateCart(elem._id, elem.plant, e.target.value, elem.price, elem.discount); elem.quantity = e.target.value; }} style={{ margin: "0 0 0 4px" }} name="quantity" id="quantity">
+                                <select value={elem.quantity} onChange={(e) => { handleUpdateCart(elem._id, e.target.value); }} style={{ margin: "0 0 0 4px" }} name="quantity" id="quantity">
                                   <option value="1">1</option>
                                   <option value="2">2</option>
                                   <option value="3">3</option>
@@ -268,7 +182,7 @@ function Cart() {
 
               }
               <div className="d-flex flex-row-reverse p-3">
-                <p className='h5'>Subtotal ({(cart ?? 0) && Number(cart.length)} item): <small className='small'>₹</small><b>{pricing && pricing.actualPriceAfterDiscount}</b></p>
+                <p className='h5'>Subtotal ({(cart ?? 0) && Number(cart.length)} item): <small className='small'>₹</small><b>{cartPriceDetails && cartPriceDetails.actualPriceAfterDiscount}</b></p>
               </div>
             </div>
             <div className="m-0 p-0 col-md-4 summary">
@@ -279,10 +193,10 @@ function Cart() {
                 <div className="row">
                   <p className="d-flex justify-content-between">
                     <small>ITEMS {(cart ?? 0) && Number(cart.length)}</small>
-                    <span><small className='small'>Subtotal ₹</small><b>{pricing && pricing.actualPriceAfterDiscount}</b></span>
+                    <span><small className='small'>Subtotal ₹</small><b>{cartPriceDetails && cartPriceDetails.actualPriceAfterDiscount}</b></span>
                   </p>
-                  <p className="text-muted small link-underline-hover" onClick={() => { handelGetListOfAddress(); setViewAddressList(!viewAddressList) }}>
-                    <small><i className="fas fa-map-marker-alt"></i> {address ? `Deliver to ${address.name.substring(0, address.name.indexOf(" "))} - ${address.city} ${address.pinCode}` : "Select delivery location"}</small>
+                  <p className="text-muted small link-underline-hover" onClick={() => {setViewAddressList(!viewAddressList) }}>
+                    <small><i className="fas fa-map-marker-alt"></i> {selectedAddress ? `Deliver to ${selectedAddress.name.substring(0, selectedAddress.name.indexOf(" "))} - ${selectedAddress.city} ${selectedAddress.pinCode}` : "Select delivery location"}</small>
                   </p>
                   <p className="text-muted mb-0">
                     <small className='small'>Have Coupon?</small>
@@ -294,13 +208,13 @@ function Cart() {
                   <p className="text-muted border-bottom pb-3">
                     <i className='fas fa-info-circle'></i>
                     {
-                      pricing && pricing.actualPriceAfterDiscount > 500 ?
+                      cartPriceDetails && cartPriceDetails.actualPriceAfterDiscount > 500 ?
                         <span className="m-0">
                           <small className='small'> Eligible for FREE Delivery. <Link>Detail</Link></small>
                         </span>
                         :
                         <span className="m-0">
-                          <small className='small'> Add items of </small><small>₹</small><b>{pricing && (500 - Number(pricing.actualPriceAfterDiscount)).toFixed(2)}</b><small> to get the for FREE Delivery <Link>Detail</Link></small>
+                          <small className='small'> Add items of </small><small>₹</small><b>{cartPriceDetails && (500 - Number(cartPriceDetails.actualPriceAfterDiscount)).toFixed(2)}</b><small> to get the for FREE Delivery <Link>Detail</Link></small>
                         </span>
                     }
 
@@ -308,23 +222,23 @@ function Cart() {
                   <div className="row border-bottom pb-2">
                     <p className="text-muted d-flex justify-content-between">
                       <small>Total : </small>
-                      <span>₹<b>{pricing && pricing.totalPriceWithoutDiscount}</b></span>
+                      <span>₹<b>{cartPriceDetails && cartPriceDetails.totalPriceWithoutDiscount}</b></span>
                     </p>
                     <p className="text-muted d-flex justify-content-between">
                       <small>Discount : </small>
-                      <span>- ₹<b>{pricing && pricing.discountPrice}</b></span>
+                      <span>- ₹<b>{cartPriceDetails && cartPriceDetails.discountPrice}</b></span>
                     </p>
                     <p className="text-muted d-flex justify-content-between">
                       <small>Delivery : </small>
-                      <span>₹<b>{pricing && pricing.deliveryPrice}</b></span>
+                      <span>₹<b>{cartPriceDetails && cartPriceDetails.deliveryPrice}</b></span>
                     </p>
                     <p className="text-muted d-flex justify-content-between">
                       <small>Subtotal : </small>
-                      <span>₹<b>{pricing && pricing.actualPriceAfterDiscount}</b></span>
+                      <span>₹<b>{cartPriceDetails && cartPriceDetails.actualPriceAfterDiscount}</b></span>
                     </p>
                   </div>
                   <div className="d-flex flex-row-reverse p-3">
-                    <p className="h5">Total: <sup>₹</sup>{pricing && pricing.totalPrice}</p>
+                    <p className="h5">Total: <sup>₹</sup>{cartPriceDetails && cartPriceDetails.totalPrice}</p>
                   </div>
                   <div className="row m-0">
                     <button onClick={handelBuyProduct} className="btn btn-success">Checkout</button>
@@ -335,7 +249,7 @@ function Cart() {
           </div>
         </div>
       </div>
-      {viewAddressList && <AddressList addressList={addressList} setSelectedAddress={setSelectedAddress} setViewAddressList={setViewAddressList} viewAddressList={viewAddressList} redirect={"/?redirect=/cart"} />}
+      {viewAddressList && <AddressList addressList={addressList} handelSelectedAddress={handelSelectedAddress} setViewAddressList={setViewAddressList} viewAddressList={viewAddressList} redirect={"/?redirect=/cart"} />}
     </section>
   )
 }
