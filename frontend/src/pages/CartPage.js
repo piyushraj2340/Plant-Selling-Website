b15b749357109a8598d3637f86c08cbde6f9a6c9
piyushraj2340 <piyushraj2340@gmail.com>
2024-03-08 21:03:17 +0530

    refactor: Restructure the Cart Page
    - Reorganized folder structure to /src/pages for better organization
    - Moved the Cart Components to the appropriate folders under the /src/pages based on web page
    - Rename the Cart Components to CartPage
    - Introduce reusable components for CartPage in /src/features/cart/Components

---
+++ frontend/src/pages/CartPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cart from '../features/cart/Components/Cart';
+
+const CartPage = () => {
+    document.title = "Cart Information";
+    return (
+        <Cart />
+    )
+}
+
+export default CartPage
