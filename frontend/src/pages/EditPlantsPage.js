b2eec93533383f25340f46e1fd2d15bca1db8f9d
piyushraj2340 <piyushraj2340@gmail.com>
2024-03-14 02:22:55 +0530

    Add EditPlantsPages and NurseryPublicStorePage with empty content

---
+++ frontend/src/pages/EditPlantsPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import EditPlants from '../features/nursery/Components/PlantsForms/EditPlants'
+
+const EditPlantsPage = () => {
+  return (
+    <>
+    <div className='d-flex justify-content-center align-items-center py-5'>
+      <p className='h1' style={{fontFamily: "cursive"}}>We are working on it...</p>
+    </div>
+    {/* <EditPlants /> */}
+    </>
+  )
+}
+
+export default EditPlantsPage
