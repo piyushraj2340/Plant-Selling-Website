1d279709351a7aaa1c7a86e8bf5ffb4d59d84649
piyushraj2340 <piyushraj2340@gmail.com>
2024-03-16 14:59:21 +0530

    refactor: update the logic auth and orderAuth middleware and handle the error globally

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -8,40 +8,42 @@ const nurseryModel = require('../model/nursery');
 
 const auth = async (req, res, next) => {
     try {
-        // request to the browser for the cookies 
+        //? request to the browser for the cookies 
         const token = req.cookies.auth;
 
-        if(!token) throw new Error("User must be logged in.");
+        //! if the token is null
+        if (!token) {
+            const error = new Error("Authentication failed");
+            error.statusCode = 401;
+            throw error;
+        };
 
-        // verify the jwt token and return the document id 
+        //? verify the jwt token and return the document id 
         const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
 
-        // find the right user from the database 
+        //? find the right user from the database 
         const user = await userModel.findOne({ _id: verifyUser._id }).select({ _id: 1, role: 1 });
 
-        // verify the user and call the next() method to validate the authentication
-        if (user) {
-            req.token = token;
-            req.user = user._id;
-            req.role = user.role;
-
-            if (req.role.includes("seller")) {
-                const nursery = await nurseryModel.findOne({ user: user._id }).select({ _id: 1 });
-                req.nursery = nursery._id;
-            }
-
-            next();
-        } else {
-            throw new Error("Authentication failed");
+        //! if user not found
+        if (!user) {
+            const error = new Error("Authentication failed");
+            error.statusCode = 401;
+            throw error;
         }
 
-    } catch (err) {
-        const info = {
-            status: false,
-            message: err.message || "Authentication failed",
+        req.token = token;
+        req.user = user._id;
+        req.role = user.role;
+
+        if (req.role.includes("seller")) {
+            const nursery = await nurseryModel.findOne({ user: user._id }).select({ _id: 1 });
+            req.nursery = nursery._id;
         }
-        console.log(err);
-        res.status(401).send(info);
+
+        next();
+
+    } catch (error) {
+        next(error); //! Pass the error to the error handling middleware
     }
 
 }
--- backend/src/middleware/orderAuth.js
+++ backend/src/middleware/orderAuth.js
@@ -5,39 +5,46 @@ const jwt = require('jsonwebtoken');
 
 const orderAuth = async (req, res, next) => {
     try {
-
+        //? request to the browser for the cookies 
         const token = req.cookies.orderSession;
 
-        if (!token) throw new Error("Invalid Order Session.");
+        //! if the token is null
+        if (!token) {
+            const error = new Error("Invalid order session token");
+            error.statusCode = 403;
+            throw error;
+        };
 
-
-        // verify the jwt token and return the document to identify the uniqueness 
+        //? verify the jwt token and return the document to identify the uniqueness 
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
 
-        // verify the user and call the next() method to validate the authentication
-        if (verifyToken) {
-
-            // checking for the same user 
-            // supposes user logged in and go to checkout session and goes logout
-            // while logged out I have removed both the cookie: user authentication and the order session 
-            // but any how the cookie of the order session not removed and new user logged then the same session will be active and this will create a issue>..
-
-            if (req.user.toString() !== verifyToken.userId.toString()) throw new Error("Invalid Order Session");
-
-            req.orderUser = req.user;
-
-            next();
-        } else {
-            throw new Error("Order Session Timed out.");
+        //! if user not found
+        if (!verifyToken) {
+            const error = new Error("Authentication failed");
+            error.statusCode = 401;
+            throw error;
         }
+
+        //? verify the user and call the next() method to validate the authentication
+
+        //? checking for the same user 
+        //? supposes user logged in and go to checkout session and goes logout
+        //? while logged out I have removed both the cookie: user authentication and the order session 
+        //? but any how the cookie of the order session not removed and new user logged then the same session will be active and this will create a issue>..
+
+        //! checking for right user to access the order session
+        if (req.user.toString() !== verifyToken.userId.toString()) {
+            const error = new Error("Invalid order session for user");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        req.orderUser = verifyToken.userId;
+
+        next();
 
     } catch (error) {
-        const info = {
-            status: false,
-            message: error.message || "Authentication failed",
-        }
-        console.log(error);
-        res.status(401).send(info);
+        next(error)
     }
 
 }
