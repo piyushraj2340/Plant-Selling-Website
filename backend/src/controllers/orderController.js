6945510dd2c1c9888ec9bd77c69fe4c5ea969ddf
piyushraj2340 <piyushraj2340@gmail.com>
2024-03-29 17:22:01 +0530

    feat(orderController): Add Confirm order after payment succeeded

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -1,4 +1,7 @@
+const mongoose = require('mongoose');
 const ordersModel = require('../model/orders');
+const cartModel = require('../model/cart');
+const { kv } = require('@vercel/kv'); //? Import the appropriate Redis client library :: TO REMOVE THE KV: DATA AFTER PAYMENT SUCCEEDED
 
 exports.createOrder = async (req, res, next) => {
     try {
@@ -26,7 +29,7 @@ exports.createOrder = async (req, res, next) => {
 
 exports.getOrderHistory = async (req, res, next) => {
     try {
-        const result = await ordersModel.find({ user: req.user });
+        const result = await ordersModel.find({ user: req.user }).select('-payment.paymentId -delivery').sort({ _id: -1 });
 
         if (!result) {
             const error = new Error("Order not found.");
@@ -52,7 +55,7 @@ exports.getOrderById = async (req, res, next) => {
     try {
         const _id = req.params.id;
 
-        const result = await ordersModel.findOne({ _id, user: req.user });
+        const result = await ordersModel.findOne({ _id, user: req.user }).select('-payment.paymentId -delivery');
 
         if (!result) {
             const error = new Error("Order not found.");
@@ -71,28 +74,163 @@ exports.getOrderById = async (req, res, next) => {
     }
 };
 
+//? This route is only accessible when payments are confirmed
 
-exports.updateOrder = async (req, res, next) => {
+exports.confirmOrderPayment = async (req, res, next) => {
+    const session = await mongoose.startSession();
     try {
-        const _id = req.params.id;
+        session.startTransaction();
 
-        const result = await ordersModel.findOneAndUpdate({ _id, user: req.user }, req.body, {
-            new: true
-        });
+        const { paymentId, status } = req.body;
+
+        if (!paymentId || !status || status !== 'succeeded') {
+            const error = new Error("You are not allowed to access this route.");
+            error.statusCode = 403;
+            throw error;
+        }
+
+        const result = await ordersModel.findOneAndUpdate(
+            { "payment.paymentId": paymentId, user: req.user },
+            {
+                $set: {
+                    "payment.status": status,
+                    "payment.message": "Payment Succeeded"
+                }
+            },
+            {
+                new: true,
+                session
+            }
+        ).select('-payment.paymentId -delivery');
 
         if (!result) {
-            const error = new Error("Order not Found.");
+            const error = new Error("Order not found.");
             error.statusCode = 404;
             throw error;
         }
 
+        //* CLEANUP_TASK:: REMOVE ALL THE MATCHING THE CART DATA FROM THE DB 
+        const deleteCartPromises = result.orderItems.map(async (items) => {
+            const deleteCartInfo = await cartModel.findOneAndDelete(
+                { plant: items.plant, user: req.user },
+                { session }
+            );
+            if (!deleteCartInfo) {
+                throw new Error("Cart not found.");
+            }
+        });
+
+        await Promise.all(deleteCartPromises);
+
+        //* CLEANUP_TASK:: REMOVE THE DATA FROM THE REDIS_DB OF THE ORDER_SESSION_DATA
+        const redisKeys = [
+            `${process.env.REDIS_VERCEL_KV_DB}:${req.user}:cartOrProducts`,
+            `${process.env.REDIS_VERCEL_KV_DB}:${req.user}:shipping`,
+            `${process.env.REDIS_VERCEL_KV_DB}:${req.user}:pricing`,
+            `${process.env.REDIS_VERCEL_KV_DB}:${req.user}:payment`
+        ];
+
+        const deleteRedisPromises = redisKeys.map(async (key) => {
+            await kv.json.del(key);
+        });
+
+        await Promise.all(deleteRedisPromises);
+
+        //* CLEANUP_TASK:: REMOVE THE ORDER_SESSION
+        //? remove the order auth session
+        res.clearCookie('orderSession', {
+            sameSite: 'none',
+            secure: true
+        });
+
+        await session.commitTransaction();
         const info = {
             status: true,
-            message: "Successfully Updated Your order.",
+            message: "Payment Succeeded",
             result
         };
         res.status(200).send(info);
     } catch (error) {
+        await session.abortTransaction();
         next(error);
+    } finally {
+        await session.endSession();
     }
 };
+
+
+// exports.confirmOrderPayment = async (req, res, next) => {
+//     const session = await mongoose.startSession();
+//     try {
+//         session.startTransaction();
+
+//         const { paymentId, status } = req.body;
+
+//         if (!paymentId || !status || status !== 'succeeded') {
+//             const error = new Error("You Are not allowed to access this route.");
+//             error.statusCode = 403;
+//             throw error;
+//         }
+
+//         const result = await ordersModel.findOneAndUpdate({ "payment.paymentId": paymentId, user: req.user }, {
+//             $set: {
+//                 "payment.status": status,
+//                 "payment.message": "Payment Succeeded"
+//             }
+//         }, {
+//             new: true,
+//             session
+//         }).select('-payment.paymentId -delivery');
+
+
+//         if (!result) {
+//             const error = new Error("Order not Found.");
+//             error.statusCode = 404;
+//             throw error;
+//         }
+
+
+//         //* CLEANUP_TASK:: REMOVE ALL THE MATCHING THE CART DATA FROM THE DB
+//         result.orderItems.forEach(async (items) => {
+//             const deleteCartInfo = await cartModel.findOneAndDelete({ plant: items.plant, user: req.user }, { session });
+
+//             if (!deleteCartInfo) {
+//                 const error = new Error("cart not Found.");
+//                 error.statusCode = 404;
+//                 throw error;
+//             }
+//         });
+
+//         //* CLEANUP_TASK:: REMOVE THE DATA FROM THE REDIS_DB OF THE ORDER_SESSION_DATA :: CART_INFORMATION
+//         await kv.json.del(`${process.env.REDIS_VERCEL_KV_DB}:${req.user}:cartOrProducts`);
+
+//         //* CLEANUP_TASK:: REMOVE THE DATA FROM THE REDIS_DB OF THE ORDER_SESSION_DATA :: SHIPPING_INFORMATION
+//         await kv.json.del(`${process.env.REDIS_VERCEL_KV_DB}:${req.user}:shipping`);
+
+//         //* CLEANUP_TASK:: REMOVE THE DATA FROM THE REDIS_DB OF THE ORDER_SESSION_DATA :: PRICING_INFORMATION
+//         await kv.json.del(`${process.env.REDIS_VERCEL_KV_DB}:${req.user}:pricing`);
+
+//         //* CLEANUP_TASK:: REMOVE THE DATA FROM THE REDIS_DB OF THE ORDER_SESSION_DATA :: PAYMENT_INFORMATION
+//         await kv.json.del(`${process.env.REDIS_VERCEL_KV_DB}:${req.user}:payment`);
+
+//         //* CLEANUP_TASK:: REMOVE THE ORDER_SESSION
+//         //? remove the order auth session
+//         res.clearCookie('orderSession', {
+//             sameSite: 'none',
+//             secure: true
+//         });
+
+//         await session.commitTransaction();
+//         const info = {
+//             status: true,
+//             message: "Payment Succeeded",
+//             result
+//         };
+//         res.status(200).send(info);
+//     } catch (error) {
+//         await session.abortTransaction();
+//         next(error);
+//     } finally {
+//         await session.endSession();
+//     }
+// };
