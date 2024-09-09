53c13078cc899d6733558aa93bcfebf5a66381e8
Piyush Raj <piyushraj2340@gmail.com>
2024-09-10 00:42:02 +0530

    Add User Verification Email Sent page and update related components
    - Added UserVerificationEmailSent.js to display a message informing users that a verification email has been sent.
    - Modified App.js to include routing for the new User Verification Email Sent page.
    - Updated Style.scss to improve the design of the verification page.
    - Enhanced authSlice.js to handle data fetching for user verification.
    - Updated LoginPage.js to redirect users to the verification page after login.
    
    These changes implement a user verification step, improving the account activation process and user feedback.

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,15 +3,19 @@ import Login from '../features/auth/Components/Login';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { userProfileAsync } from '../features/user/userSlice';
-
+import { resetState } from '../features/auth/authSlice'
 
 const LoginPage = () => {
     document.title = "Login";
 
     const user = useSelector(state => state.user.user);
+    const {isUserVerificationNeeded, email} = useSelector(state => state.auth);
+
     const dispatch = useDispatch();
 
     const navigate = useNavigate();
+
+    
 
     const handleGetUserData = async () => {
         !user && dispatch(userProfileAsync());
@@ -21,6 +25,14 @@ const LoginPage = () => {
             return;
         }
     }
+
+    useEffect(() => {
+        if(isUserVerificationNeeded) {
+            navigate(`/account/verificationEmail?email=${email}`);
+            dispatch(resetState());
+            return;
+        }
+    }, [dispatch, isUserVerificationNeeded])
 
     useEffect(() => {
         handleGetUserData();
