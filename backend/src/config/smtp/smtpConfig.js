6a15fc1859dac0892338ad75cb6c35f95675f3c9
Piyush Raj <piyushraj2340@gmail.com>
2024-09-11 00:32:07 +0530

    Fix: Set secure property based on SMTP port

--- backend/src/config/smtp/smtpConfig.js
+++ backend/src/config/smtp/smtpConfig.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const smtpConfig = {
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false, // true for 465, false for other ports
+  secure: process.env.SMTP_PORT == 465 ? true : false, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_GOOGLE_MAIL_ADDRESS,
     pass: process.env.SMTP_GOOGLE_APP_PASSWORD,
