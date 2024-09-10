2b94cccbf45e59208f71cd022549315a44b9ad08
Piyush Raj <piyushraj2340@gmail.com>
2024-09-11 00:00:10 +0530

    Add Account Verification UI and logic
    Implemented account verification flow, including multiple verification states. Added new components and updated existing files to handle the process, from verifying the account to handling invalid or expired tokens.
    
    - Added new route in App.js for the UserVerificationConfirmAccount page
    - Updated Style.scss with styles for AccountVerificationCompleted, InvalidVerificationToken, and VerificationConfirmAccount components
    - Enhanced authHelper.js to reset the authSlice state (loading, authValidation, etc.)
    - Updated authSlice.js to handle API calls and update the state based on the account verification process
    
    New components added:
    - AccountVerificationCompleted.js: Displays a message confirming account verification
    - InvalidVerificationToken.js: Displays an error message for expired or invalid tokens
    - VerificationConfirmAccount.js: Allows users to confirm their account by clicking a verify button
    - UserVerificationConfirmAccount.js: Main page that conditionally renders the verification components based on user status
    
    These changes implement a comprehensive account verification system with improved user feedback and error handling.

--- frontend/src/features/auth/Components/utils/authHelper.js
+++ frontend/src/features/auth/Components/utils/authHelper.js
@@ -1,9 +1,99 @@
-// export const handelAuthStatus = (asyncStatus, message, reducer, clearLogStatus) => {
-//     if (asyncStatus && asyncStatus.status) {
-//         message.success(asyncStatus.message);
-//     } else if (asyncStatus && !asyncStatus.status) {
-//         message.error(asyncStatus.message);
-//     } 
+import { message } from "antd";
 
-//     reducer(clearLogStatus());
-// }
+const loadingRestAuthStore = (state) => {
+    //! Checking for invalid state or state is null 
+    if (!state) {
+        message.error("Invalid State information for authSlice");
+        return;
+    }
+
+    //^ Reset the other values 
+    state.userAuthCheck = false;
+    state.isUserVerificationNeeded = false;
+    state.email = '';
+    state.isValidToken = false;
+    state.verificationCompleted = false;
+    state.error = null;
+
+    //* Only set the loading state 
+    state.isLoading = true;
+}
+
+const trueAuthCheckResetAuthStore = (state) => {
+    //! Checking for invalid state or state is null 
+    if (!state) {
+        message.error("Invalid State information for authSlice");
+        return;
+    }
+
+    //^ Reset the other values 
+    state.isUserVerificationNeeded = false;
+    state.email = '';
+    state.isValidToken = false;
+    state.verificationCompleted = false;
+    state.error = null;
+    state.isLoading = false;
+    
+    //* Only set the user Auth Check state true  
+    state.userAuthCheck = true;
+}
+
+
+const resetToDefaultAuthStore = (state) => {
+    //! Checking for invalid state or state is null 
+    if (!state) {
+        message.error("Invalid State information for authSlice");
+        return;
+    }
+
+    //^ Reset all the values 
+    state.isUserVerificationNeeded = false;
+    state.email = '';
+    state.isValidToken = false;
+    state.verificationCompleted = false;
+    state.error = null;
+    state.isLoading = false;
+    state.userAuthCheck = false;
+}
+
+const userAccountVerificationAuthStore = (state) => {
+    //! Checking for invalid state or state is null 
+    if (!state) {
+        message.error("Invalid State information for authSlice");
+        return;
+    }
+
+    //^ Reset the other values 
+    state.isUserVerificationNeeded = false;
+    state.email = '';
+    state.isValidToken = false;
+    state.error = null;
+    state.userAuthCheck = false;
+    state.isLoading = false;
+    
+    //* Only set the verificationCompleted state true  
+    state.verificationCompleted = true;
+}
+
+
+const validateVerificationTokenAuthStore = (state) => {
+    //! Checking for invalid state or state is null 
+    if (!state) {
+        message.error("Invalid State information for authSlice");
+        return;
+    }
+
+    //^ Reset the other values 
+    state.isUserVerificationNeeded = false;
+    state.email = '';
+    state.error = null;
+    state.userAuthCheck = false;
+    state.verificationCompleted = false;
+    state.isLoading = false;
+    
+    //* Only set the isValidToken state true  
+    state.isValidToken = true;
+}
+
+
+export { loadingRestAuthStore, trueAuthCheckResetAuthStore, resetToDefaultAuthStore, userAccountVerificationAuthStore, validateVerificationTokenAuthStore }
